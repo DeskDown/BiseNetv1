975852cbd9046fd99988f1f0338467e17862be9a
inam <rehman-official@outlook.com>
2021-05-08 13:18:03 +0200

    ready for training v1.1

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import sys, os
+
 sys.path.append(os.getcwd())
 
 import argparse
@@ -12,31 +13,32 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm.notebook import tqdm
 import numpy as np
 from utils import poly_lr_scheduler
-from utils import reverse_one_hot, compute_global_accuracy, fast_hist, \
-    per_class_iu
+from utils import reverse_one_hot, compute_global_accuracy, fast_hist, per_class_iu
 from loss import DiceLoss
 
 
 def get_transform():
-    train_transform = Compose([
-        RandomResizedCrop(320, (0.5, 2.0)),
-        RandomHorizontalFlip(),
-        ToTensor(),
-        Normalize(mean=[0.485, 0.456, 0.406],
-                  std=[0.229, 0.224, 0.225]),
-    ])
-    val_transform = Compose([
-        PadCenterCrop(size=512),
-        ToTensor(),
-        Normalize(mean=[0.485, 0.456, 0.406],
-                  std=[0.229, 0.224, 0.225]),
-    ])
+    train_transform = Compose(
+        [
+            RandomResizedCrop(320, (0.5, 2.0)),
+            RandomHorizontalFlip(),
+            ToTensor(),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    val_transform = Compose(
+        [
+            PadCenterCrop(size=512),
+            ToTensor(),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
 
     return train_transform, val_transform
 
 
 def val(args, model, dataloader):
-    print('start val!')
+    print("start val!")
     # label_info = get_label_info(csv_path)
     with torch.no_grad():
         model.eval()
@@ -67,8 +69,8 @@ def val(args, model, dataloader):
         miou_list = per_class_iu(hist)[:-1]
         # miou_dict, miou = cal_miou(miou_list, csv_path)
         miou = np.mean(miou_list)
-        print('precision per pixel for test: %.3f' % precision)
-        print('mIoU for validation: %.3f' % miou)
+        print("precision per pixel for test: %.3f" % precision)
+        print("mIoU for validation: %.3f" % miou)
         # miou_str = ''
         # for key in miou_dict:
         #     miou_str += '{}:{},\n'.format(key, miou_dict[key])
@@ -78,79 +80,137 @@ def val(args, model, dataloader):
 
 
 def train(args, model, optimizer, dataloader_train, dataloader_val):
-    writer = SummaryWriter(comment=''.format(args.optimizer, args.context_path))
-    if args.loss == 'dice':
+    writer = SummaryWriter(comment="".format(args.optimizer, args.context_path))
+    if args.loss == "dice":
         loss_func = DiceLoss()
-    elif args.loss == 'crossentropy':
+    elif args.loss == "crossentropy":
         loss_func = torch.nn.CrossEntropyLoss(ignore_index=255)
     max_miou = 0
     step = 0
     for epoch in range(args.num_epochs):
-        lr = poly_lr_scheduler(optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs)
+        lr = poly_lr_scheduler(
+            optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs
+        )
         model.train()
         tq = tqdm(total=len(dataloader_train) * args.batch_size)
-        tq.set_description('epoch %d, lr %f' % (epoch, lr))
+        tq.set_description("epoch %d, lr %f" % (epoch, lr))
         loss_record = []
         for i, (data, label) in enumerate(dataloader_train):
             if torch.cuda.is_available() and args.use_gpu:
                 data = data.cuda()
                 label = label.cuda().long()
 
+            label = label.type(torch.LongTensor)
             output, output_sup1, output_sup2 = model(data)
             loss1 = loss_func(output, label)
             loss2 = loss_func(output_sup1, label)
             loss3 = loss_func(output_sup2, label)
             loss = loss1 + loss2 + loss3
             tq.update(args.batch_size)
-            tq.set_postfix(loss='%.6f' % loss)
+            tq.set_postfix(loss="%.6f" % loss)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             step += 1
-            writer.add_scalar('loss_step', loss, step)
+            writer.add_scalar("loss_step", loss, step)
             loss_record.append(loss.item())
         tq.close()
         loss_train_mean = np.mean(loss_record)
-        writer.add_scalar('epoch/loss_epoch_train', float(loss_train_mean), epoch)
-        print('loss for train : %f' % (loss_train_mean))
+        writer.add_scalar("epoch/loss_epoch_train", float(loss_train_mean), epoch)
+        print("loss for train : %f" % (loss_train_mean))
         if epoch % args.checkpoint_step == 0 and epoch != 0:
             if not os.path.isdir(args.save_model_path):
                 os.mkdir(args.save_model_path)
-            torch.save(model.module.state_dict(),
-                       os.path.join(args.save_model_path, 'model.pth'))
+            torch.save(
+                model.module.state_dict(),
+                os.path.join(args.save_model_path, "model.pth"),
+            )
 
         if epoch % args.validation_step == 0 and epoch != 0:
             precision, miou = val(args, model, dataloader_val)
             if miou > max_miou:
                 max_miou = miou
                 import os
+
                 os.makedirs(args.save_model_path, exist_ok=True)
-                torch.save(model.module.state_dict(),
-                           os.path.join(args.save_model_path, 'best_dice_loss.pth'))
-            writer.add_scalar('epoch/precision_val', precision, epoch)
-            writer.add_scalar('epoch/miou val', miou, epoch)
+                torch.save(
+                    model.module.state_dict(),
+                    os.path.join(args.save_model_path, "best_dice_loss.pth"),
+                )
+            writer.add_scalar("epoch/precision_val", precision, epoch)
+            writer.add_scalar("epoch/miou val", miou, epoch)
 
 
 def main(params):
     # basic parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_epochs', type=int, default=300, help='Number of epochs to train for')
-    parser.add_argument('--epoch_start_i', type=int, default=0, help='Start counting epochs from this number')
-    parser.add_argument('--checkpoint_step', type=int, default=10, help='How often to save checkpoints (epochs)')
-    parser.add_argument('--validation_step', type=int, default=10, help='How often to perform validation (epochs)')
-    parser.add_argument('--batch_size', type=int, default=1, help='Number of images in each batch')
-    parser.add_argument('--context_path', type=str, default="resnet101",
-                        help='The context path model you are using, resnet18,resnet50, resnet101.')
-    parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate used for train')
-    parser.add_argument('--data', type=str, default='data', help='path of training data')
-    parser.add_argument('--num_workers', type=int, default=4, help='num of workers')
-    parser.add_argument('--num_classes', type=int, default=32, help='num of object classes (with void)')
-    parser.add_argument('--cuda', type=str, default='0', help='GPU ids used for training')
-    parser.add_argument('--use_gpu', type=bool, default=True, help='whether to user gpu for training')
-    parser.add_argument('--pretrained_model_path', type=str, default=None, help='path to pretrained model')
-    parser.add_argument('--save_model_path', type=str, default="checkpoints", help='path to save model')
-    parser.add_argument('--optimizer', type=str, default='rmsprop', help='optimizer, support rmsprop, sgd, adam')
-    parser.add_argument('--loss', type=str, default='crossentropy', help='loss function, dice or crossentropy')
+    parser.add_argument(
+        "--num_epochs", type=int, default=300, help="Number of epochs to train for"
+    )
+    parser.add_argument(
+        "--epoch_start_i",
+        type=int,
+        default=0,
+        help="Start counting epochs from this number",
+    )
+    parser.add_argument(
+        "--checkpoint_step",
+        type=int,
+        default=10,
+        help="How often to save checkpoints (epochs)",
+    )
+    parser.add_argument(
+        "--validation_step",
+        type=int,
+        default=10,
+        help="How often to perform validation (epochs)",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=1, help="Number of images in each batch"
+    )
+    parser.add_argument(
+        "--context_path",
+        type=str,
+        default="resnet101",
+        help="The context path model you are using, resnet18,resnet50, resnet101.",
+    )
+    parser.add_argument(
+        "--learning_rate", type=float, default=0.01, help="learning rate used for train"
+    )
+    parser.add_argument(
+        "--data", type=str, default="data", help="path of training data"
+    )
+    parser.add_argument("--num_workers", type=int, default=4, help="num of workers")
+    parser.add_argument(
+        "--num_classes", type=int, default=32, help="num of object classes (with void)"
+    )
+    parser.add_argument(
+        "--cuda", type=str, default="0", help="GPU ids used for training"
+    )
+    parser.add_argument(
+        "--use_gpu", type=bool, default=True, help="whether to user gpu for training"
+    )
+    parser.add_argument(
+        "--pretrained_model_path",
+        type=str,
+        default=None,
+        help="path to pretrained model",
+    )
+    parser.add_argument(
+        "--save_model_path", type=str, default="checkpoints", help="path to save model"
+    )
+    parser.add_argument(
+        "--optimizer",
+        type=str,
+        default="rmsprop",
+        help="optimizer, support rmsprop, sgd, adam",
+    )
+    parser.add_argument(
+        "--loss",
+        type=str,
+        default="crossentropy",
+        help="loss function, dice or crossentropy",
+    )
 
     args = parser.parse_args(params)
 
@@ -164,38 +224,40 @@ def main(params):
         batch_size=args.batch_size,
         shuffle=True,
         num_workers=args.num_workers,
-        drop_last=True
+        drop_last=True,
     )
     dataset_val = VOC(train_path, image_set="val", transform=val_transform)
     dataloader_val = DataLoader(
         dataset_val,
         batch_size=args.batch_size,
         shuffle=True,
-        num_workers=args.num_workers
+        num_workers=args.num_workers,
     )
 
     # build model
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
     model = BiSeNet(args.num_classes, args.context_path)
     if torch.cuda.is_available() and args.use_gpu:
         model = model.cuda()
 
     # build optimizer
-    if args.optimizer == 'rmsprop':
+    if args.optimizer == "rmsprop":
         optimizer = torch.optim.RMSprop(model.parameters(), args.learning_rate)
-    elif args.optimizer == 'sgd':
-        optimizer = torch.optim.SGD(model.parameters(), args.learning_rate, momentum=0.9, weight_decay=1e-4)
-    elif args.optimizer == 'adam':
+    elif args.optimizer == "sgd":
+        optimizer = torch.optim.SGD(
+            model.parameters(), args.learning_rate, momentum=0.9, weight_decay=1e-4
+        )
+    elif args.optimizer == "adam":
         optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
     else:  # rmsprop
-        print('not supported optimizer \n')
+        print("not supported optimizer \n")
         return None
 
     # load pretrained model if exists
     if args.pretrained_model_path is not None:
-        print('load model from %s ...' % args.pretrained_model_path)
+        print("load model from %s ..." % args.pretrained_model_path)
         model.module.load_state_dict(torch.load(args.pretrained_model_path))
-        print('Done!')
+        print("Done!")
 
     # train
     train(args, model, optimizer, dataloader_train, dataloader_val)
@@ -203,19 +265,29 @@ def main(params):
     val(args, model, dataloader_val)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     params = [
-        '--num_epochs', '30',
-        '--learning_rate', '1e-3',
-        '--data', 'data',
-        '--num_workers', '8',
-        '--num_classes', '21',
-        '--cuda', '0',
-        '--use_gpu', 'False',
-        '--batch_size', '16',
-        '--save_model_path', './checkpoints_18_sgd',
-        '--context_path', 'resnet18',  # set resnet18,resnet50 or resnet101
-        '--optimizer', 'sgd',
+        "--num_epochs",
+        "30",
+        "--learning_rate",
+        "1e-3",
+        "--data",
+        "/gdrive/MyDrive/data",
+        "--num_workers",
+        "8",
+        "--num_classes",
+        "21",
+        "--cuda",
+        "0",
+        "--use_gpu",
+        "False",
+        "--batch_size",
+        "16",
+        "--save_model_path",
+        "./checkpoints_18_sgd",
+        "--context_path",
+        "resnet18",  # set resnet18,resnet50 or resnet101
+        "--optimizer",
+        "sgd",
     ]
     main(params)
-
